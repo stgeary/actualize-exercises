0c9134a36b3be6402882024289f8d33d1860cb67
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-18 11:37:14 -0600

    Add files via upload

---
+++ 111.rb
@@ -0,0 +1,10 @@
+#Q1
+puts 2+2
+puts 180/6
+puts 3*4
+puts 984-1
+
+#Q2
+puts 17/0
+#ZeroDivisionError
+#In math you can not divide a number by 0, so it causes the program to crash and have an error
