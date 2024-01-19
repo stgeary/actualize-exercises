02d8ff4ae129ff882f554b58f4cdc2f3bba8e808
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 13:04:35 -0600

    Add files via upload

---
+++ 121.rb
@@ -0,0 +1,8 @@
+#Q1
+puts 2908490.to_s #Changes the interger to a string
+puts "3".to_i #Changes the string to an interger
+puts "hi".upcase #Changes all letters in the string to be uppercase
+
+#Q2
+Integer.sqrt(-1)
+#You can not take a square root of a negative number
