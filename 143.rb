0191ba78fcc89a5f7e5f6e2c166594cdd53916f4
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 14:59:47 -0600

    Add files via upload

---
+++ 143.rb
@@ -0,0 +1,10 @@
+#Q1
+city_populations = {"Chicago" => 2700000, "New York City" => 8400000, "San Francisco" => 800000}
+pp city_populations
+
+#Q2
+#2nd line switch around 2 and "dogs"
+my_pets = { "dogs" => 2, "cats" => 1, "fish" => 5 }
+if my_pets["dogs"] == 2
+  puts "I have 2 dogs!"
+end
