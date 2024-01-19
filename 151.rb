b90368d0361885e0035eed7d8a55415aabc39002
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 15:18:59 -0600

    Add files via upload

---
+++ 151.rb
@@ -0,0 +1,33 @@
+#Q1
+def x
+   puts" _________                     ____"                     
+   puts" /   _____/_____   ____   ____ |  | _____.__."            
+   puts" \_____  \\____ \ /  _ \ /  _ \|  |/ <   |  |"            
+   puts" /        \  |_> >  <_> |  <_> )    < \___  |"            
+   puts"/_______  /   __/ \____/ \____/|__|_ \/ ____|"            
+   puts"        \/|__|                      \/\/"            
+   puts"  _________"            
+   puts" /   _____/ ____ _____ _______ ___.__."            
+   puts" \_____  \_/ ___\\__  \\_  __ <   |  |"            
+   puts" /        \  \___ / __ \|  | \/\___  |"            
+   puts"/_______  /\___  >____  /__|   / ____|"            
+   puts"        \/     \/     \/       \/"            
+   puts"  ___________          .__          __"            
+   puts" /   _____/  | __ ____ |  |   _____/  |_  ____   ____   ______"
+   puts" \_____  \|  |/ // __ \|  | _/ __ \   __\/  _ \ /    \ /  ___/"
+   puts" /        \    <\  ___/|  |_\  ___/|  | (  <_> )   |  \\___ \ "
+   puts"/_______  /__|_ \\___  >____/\___  >__|  \____/|___|  /____  >"
+   puts"        \/     \/    \/          \/                 \/     \/"
+end
+x
+x
+x
+
+#Q2
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+print_lyrics
