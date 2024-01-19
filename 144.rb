641a1bf1539c7ebc36b5109a4e0f6f7f1d91467e
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 15:06:58 -0600

    Add files via upload

---
+++ 144.rb
@@ -0,0 +1,14 @@
+#Q1
+cars = [
+    {"make" => "Honda Accord", "color" => "Silver", "year" => 2006},
+    {"make" => "Toyota Camry", "color" => "Red", "year" => 2022},
+    {"make" => "Honda Civic", "color" => "Black", "year" => 2013}
+]
+
+#Q2
+students = [
+  { "name" => "Maria", "age" => 20, "major" => "Computer Science" },
+  { "name" => "Sofia", "age" => 21, "major" => "Mathematics" },
+  { "name" => "Jayden", "age" => 19, "major" => "Biology" }
+]
+pp students[0]["age"]
