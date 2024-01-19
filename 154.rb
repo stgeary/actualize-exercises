7077d4652bf9ac3f97c2048b038bbe44ce9933cd
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 16:18:19 -0600

    Add files via upload

---
+++ 154.rb
@@ -0,0 +1,37 @@
+#Q1
+class Song
+    def initialize(song_title, song_artist, song_lyrics)
+        @title = song_title
+        @artist = song_artist
+        @lyrics = song_lyrics
+    end
+
+    def title
+        return @title
+    end
+
+    def artist
+        return @artist
+    end
+
+    def lyrics
+        return @lyrics
+    end
+end
+
+#Q2
+class Person
+    def initialize(input_name, input_age, input_occupation)
+      @name = input_name
+      @age = input_age
+      @occupation = input_occupation
+    end
+  
+    def get_name
+      return @name
+    end
+end
+  
+  person = Person.new("Shawn", 42, "accountant")
+  pp person
+  pp Person.get_name
