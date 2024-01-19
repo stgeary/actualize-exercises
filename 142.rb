e55a7517a8883fb0d6d8dd1f90d72b8d909ed23c
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-19 14:51:10 -0600

    Add files via upload

---
+++ 142.rb
@@ -0,0 +1,16 @@
+#Q1
+x = ["Chicken", "Tea", "Tuxedo", "Club Penguin"]
+y = 0
+while y < 4
+    puts x[y]
+    y = y + 1
+end
+
+#Q2
+a = [17, 32495709, 329, 599]
+b = 0
+while b < 4
+    c = a[b] * 3
+    puts c
+    b = b + 1
+end
