12b09e54bd369802ef899010afbdd8be958bb0d0
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-01-24 11:12:45 -0600

    Add files via upload

--- 112.rb
+++ 112.rb
@@ -1,18 +1,18 @@
 #Q1
-puts "hello" + "hello" + "hello"
-puts "hello"*3
+#puts "hello" + "hello" + "hello"
+#puts "hello"*3
 
 #Q2
 #Commented code I predict will not work
 puts 9 + 3
 #puts "9" + 3
 #puts 9 + "3"
-#puts "9" + "3"
+puts "9" + "3"
 puts 9 - 3
 #puts "9" - 3
 #puts 9 - "3"
 puts 9 * 3
-#puts "9" * 3
+puts "9" * 3
 #puts 9 * "3"
 #puts "9" * "3"
 puts 9 / 3
