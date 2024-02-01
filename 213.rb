6cbfd2b28493ee338ea64278aa7f2a29d2aa320a
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-02-01 12:33:51 -0600

    Add files via upload

---
+++ 213.rb
@@ -0,0 +1,10 @@
+#Q1
+first_name = "Katherine"
+last_name = "Johnson"
+puts #{first_name}" "#{last_name}" was a NASA research mathematician."
+
+
+#Q2
+name = "Salvador"
+age = 37
+puts name + " is " + age.to_s + " years old and goes by 'Chava' with his friends."
