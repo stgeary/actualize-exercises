9fd0d313f4f5426235dc9e0b5a232c7add761b15
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-02-01 12:06:58 -0600

    Add files via upload

---
+++ 212.rb
@@ -0,0 +1,11 @@
+#Q1
+a = 17
+b = "Bear"
+c = a.to_s + b
+puts c
+
+#Q2
+#The error is in the order, puts can't print a variable that doesn't exist yet so you have to reorder the code
+x = 10
+y = x
+puts x + y
