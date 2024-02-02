f309467b5370f77e421d655b9b9a51e023794f62
Scott Geary <156706847+stgeary@users.noreply.github.com>
2024-02-02 11:44:55 -0600

    Add files via upload

---
+++ 231.rb
@@ -0,0 +1,12 @@
+# Problem: Write a method that takes in a string and returns the string with all capital letters. Then run the method and print the result.
+
+s = "silver lining"
+
+def caps (string)
+    a = string.upcase
+    puts a
+end
+
+caps(s)
+
+#I was able to easily solve this from memory, will work wish hashses more next
